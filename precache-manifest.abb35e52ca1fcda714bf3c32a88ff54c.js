ea75deb3ee48388b5b6a29ff9ab14b2fd03dc465
Ivan Rogelio Chenoweth Chenoweth <ivan chenoweth@tiempodev.local>
2020-04-04 20:23:20 -0700

    Updates

---
+++ precache-manifest.abb35e52ca1fcda714bf3c32a88ff54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ac082ff2096b79e9d339d521912c3e",
+    "url": "/udemy-react-course/index.html"
+  },
+  {
+    "revision": "6a4e27959a3d10bc3442",
+    "url": "/udemy-react-course/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/udemy-react-course/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "6a4e27959a3d10bc3442",
+    "url": "/udemy-react-course/static/js/main.1ee783f1.chunk.js"
+  },
+  {
+    "revision": "0164b550824f9687aa18",
+    "url": "/udemy-react-course/static/js/runtime~main.805353c0.js"
+  }
+]);
